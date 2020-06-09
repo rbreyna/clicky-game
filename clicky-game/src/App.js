fd1f3c7bd57946225d738efafa7fc7f374291b86
Roy Reyna <royreyna7@yahoo.com>
2020-06-09 13:23:04 -0500

    images shuffle

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -7,26 +7,47 @@ import characters from "./memory-cards.json"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
+
     state = {
         characters,
         topScore: 0,
         currentScore: 0,
     };
 
-    render() {
-        return (
+    shuffleArray = arr => {
 
+        var counter = arr.length
+        var random;
+        var temp;
+
+        // While there are elements in the array
+        while (counter > 0) {
+            // Pick a random index
+            random = Math.floor(Math.random() * counter);
+            // Decrease ctr by 1
+            counter--;
+            // And swap the last element with it
+            temp = arr[counter];
+            arr[counter] = arr[random];
+            arr[random] = temp;
+        }
+        return arr;
+    }
+
+    render() {
+
+
+        return (
             <Wrapper className="bg-dark">
                 <Nav />
                 <div className="container col-xl-6" >
-                <Col >
-                    {this.state.characters.map(persona =>
-                        <Cards image={persona.image}></Cards>)}
-                </Col>
+                    <Col >
+                        {this.shuffleArray(this.state.characters).map(persona =>
+                            <Cards image={persona.image}></Cards>)}
+                    </Col>
                 </div>
             </Wrapper >
-           
         )
     }
 }
-export default App;
+    export default App;
