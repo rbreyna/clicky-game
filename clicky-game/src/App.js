2c93935db79b6e9947249a9c31bfa76d538ca82b
Roy Reyna <royreyna7@yahoo.com>
2020-06-09 13:10:32 -0500

    Image cards displaying and formatted

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import Wrapper from "./components/Wrapper"
+import Col from "react-bootstrap/Col"
 import Nav from "./components/Navbar"
 import Cards from "./components/Cards"
+import characters from "./memory-cards.json"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App(){
+class App extends Component {
+    state = {
+        characters,
+        topScore: 0,
+        currentScore: 0,
+    };
 
-    return(
-        <Wrapper>
-          <Nav/>
-          <Cards/>
-        </Wrapper>
+    render() {
+        return (
 
-    )
+            <Wrapper className="bg-dark">
+                <Nav />
+                <div className="container col-xl-6" >
+                <Col >
+                    {this.state.characters.map(persona =>
+                        <Cards image={persona.image}></Cards>)}
+                </Col>
+                </div>
+            </Wrapper >
+           
+        )
+    }
 }
-
 export default App;
--- clicky-game/src/components/Cards.js
+++ clicky-game/src/components/Cards.js
@@ -1,8 +1,12 @@
 import React from "react"
+import Container from "react-bootstrap/Container"
+
 import Image from "react-bootstrap/Image"
 
-function Cards(){
-    return 
+function Cards(props){
+    return(<Image className="m-1" lg={3} src={props.image} width={150} height={150} rounded/>)    
 }
+
+
 
 export default Cards;
